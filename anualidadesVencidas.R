e0ebf7760fac82092c722c3bd9c601a882911f65
IsabellaC01 <isabellacampos228@gmail.com>
2024-10-20 19:45:23 -0600

    Update anualidadesVencidas.R

--- anualidadesVencidas.R
+++ anualidadesVencidas.R
@@ -12,32 +12,26 @@ anualidad=function(VF,r, n) {
 }
 
 # Número de pagos dada el valor futuro, tasa y anualidad
-plazo_por_valor_futuro <- function(valor_futuro, anualidad, tasa) {
-  n <- log((valor_futuro * tasa / anualidad) + 1) / log(1 + tasa)
-  return(ceiling(n))  # Redondear hacia arriba
-}
-
-# Tasa del periodo dada el valor futuro, plazo y anualidad
-tasa_por_valor_futuro <- function(valor_futuro, anualidad, n) {
-  f <- function(tasa) {
-    valor_futuro - anualidad * ((1 + tasa)^n - 1) / tasa
-  }
-  return(uniroot(f, c(0, 1))$root)
+plazo=function(VF,A,r) {
+  n=log((VF*r/A) + 1)/log(1+r)
+  return(n)  
 }
 
 # Valor actual de una anualidad vencida
-valor_actual_anualidad <- function(anualidad, tasa, n) {
-  return(anualidad * (1 - (1 + tasa)^(-n)) / tasa)
+valorActual=function(A,r,n) {
+  VA=(A(((1+r)^n)-1))/(r((1+r)^n))
+  return(VA)
 }
 
 # Anualidad dada el valor actual
-anualidad_por_valor_actual <- function(valor_actual, tasa, n) {
-  return(valor_actual * tasa / (1 - (1 + tasa)^(-n)))
+anualidadValorActual=function(Va,r,n) {
+  A=(r(VA((1+r)^n)))/(((1+r)^t)-1)
+  return(A)
 }
 
 # Número de pagos dada el valor actual, tasa y anualidad
 plazo_por_valor_actual <- function(valor_actual, anualidad, tasa) {
-  n <- log(anualidad / (anualidad - valor_actual * tasa)) / log(1 + tasa)
+  n=log(anualidad / (anualidad - valor_actual * tasa)) / log(1 + tasa)
   return(ceiling(n))  # Redondear hacia arriba
 }
 
